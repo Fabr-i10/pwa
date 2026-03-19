1351c97d0a427d3537f5df2244d51cdfdf352e06
Fabr-i10 <fabricio20arg@gmail.com>
2026-03-19 12:09:49 -0600

    fix v2

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
-if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('/serviceworker.js')
-    .then( reg=>{
-        console.log('Service Worker se ha instalado correctamente. Scope:',reg.scope)
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("./serviceworker.js")
+    .then((reg) => {
+      console.log(
+        "Service Worker se ha instalado correctamente. Scope:",
+        reg.scope,
+      );
     })
-    .catch(err=>{
-        console.log('Error al instalar el SW: ',err)
-    })
-}else{
-    console.log('Este navegador no soporta Service Workers')
+    .catch((err) => {
+      console.log("Error al instalar el SW: ", err);
+    });
+} else {
+  console.log("Este navegador no soporta Service Workers");
 }
